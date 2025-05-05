2706fc89ce2a3ba74fc86d648e787c67e0a279ae
Stefan Wagner <71991771+stefanwagner19@users.noreply.github.com>
2025-05-05 15:47:52 -0700

    update

--- mysettings.py
+++ mysettings.py
@@ -2,27 +2,29 @@ class PacmanRewards:
 
     @staticmethod
     def perMove():
-        return 1000
+        return 0
 
     @staticmethod
     def onEatingFood():
-        return 100
+        return 0
 
     @staticmethod
     def onEatingGhost():
-        return 200
+        return 0
 
     @staticmethod
     def onWin():
-        return 500
+        return 0
 
     @staticmethod
     def onLoss():
-        return -500
+        return 0
 
 
 normalRuns = 10
 trainingRuns = 2000
+grid = "smallGrid"
+# grid = "customGrid"
 
 if __name__ == "__main__":
     from pacmanRL.run_helper import run
--- pacmanRL/run_helper.py
+++ pacmanRL/run_helper.py
@@ -14,12 +14,13 @@ def run():
     rewards = mysettings.PacmanRewards()
     N_runs = mysettings.normalRuns
     N_training = mysettings.trainingRuns
+    grid = mysettings.grid
     args = pacman.readCommand(
         [
             "-p",
             "PacmanQAgent",
             f"--numGames={N_runs + N_training}",
-            "--layout=smallGrid",
+            f"--layout={grid}",
             f"--numTraining={N_training}",
         ]
     )
